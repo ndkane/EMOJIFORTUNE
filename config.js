c913e114a7922fd06ed5c1dff68c6ffbd6f2363c
ndkane <3464509+ndkane@users.noreply.github.com>
2018-02-06 21:03:15 +0000

    config redact

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'JKYACLmSZJsfQUKD4kEENEwpd',
-  consumer_secret:      'zskkCnzoOtLI9fdGnI5Q46XZNo4XljLGlpsiaUYYdfYKN5H7db',
-  access_token:         '936336238679863296-azYL3rbZ0aalCAhIr3OySPDCCyBUtBP',
-  access_token_secret:  'sqd4yrH6FU21cXG2gIa8VXNEncTLYIAPPIAAvlr4Lannz'
+  consumer_key:         'uY7dwaeeaorpiFcLyPnSYlKJ8',
+  consumer_secret:      'LkFmIRFHmFwl7D16Zrigz07defgLDiY0wxqcrqKmdZaY2bsGlQ',
+  access_token:         '936336238679863296-hOnRUgAPRaLUlI0lZdse6hNu1YFbabB',
+  access_token_secret:  'i3NINBeYbCm2rTgXJyWU9GnycKIzEhe8IAWwa13Jdrdfx'
 };
