6f0d6e3039eb048fa938b503a04918337be14b56
ndkane <3464509+ndkane@users.noreply.github.com>
2018-02-06 21:09:13 +0000

    redact

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'uY7dwaeeaorpiFcLyPnSYlKJ8',
-  consumer_secret:      'LkFmIRFHmFwl7D16Zrigz07defgLDiY0wxqcrqKmdZaY2bsGlQ',
-  access_token:         '936336238679863296-hOnRUgAPRaLUlI0lZdse6hNu1YFbabB',
-  access_token_secret:  'i3NINBeYbCm2rTgXJyWU9GnycKIzEhe8IAWwa13Jdrdfx'
-};
